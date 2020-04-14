3c2e05ad0798f1a1ae6c379655b7cc0b7755616f
Jsalvadorpp <josesalvadorpp@hotmail.com>
2020-04-14 18:05:50 -0400

    users profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+    def user_is_permitted?
+        (user_signed_in? && current_user == @user)? true:false
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users , controllers: { omniauth_callbacks: 'users/omniauth_callbacks' ,
                                       registrations: 'users/profile'}
+                                    
+  get '/user/profile/:id' => 'user#show'
   #devise_scope :user do
     #get '/users/sign_out' => 'devise/sessions#destroy'
   #end
