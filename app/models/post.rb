4cdec7f735c84ecc9cc6587ce5cb9beeed3f4451
Jsalvadorpp <josesalvadorpp@hotmail.com>
2020-04-23 23:38:51 -0400

    add relationships between likes and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
     belongs_to :creator, class_name: :User , foreign_key: :creator_id
+    has_many :likes , foreign_key: :liked_post_id
+    
     default_scope { order("created_at DESC") }
 end
