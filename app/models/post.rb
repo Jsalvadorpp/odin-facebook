e51d3b66809ca5c6cf9edbe1dc7ecd1bfd60d91e
Jsalvadorpp <josesalvadorpp@hotmail.com>
2020-04-25 16:41:35 -0400

    create comments model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
     belongs_to :creator, class_name: :User , foreign_key: :creator_id
     has_many :likes , foreign_key: :liked_post_id
+
+    has_many :comments
     
     default_scope { order("created_at DESC") }
 end
